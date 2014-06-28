d7f845c52e5a1f5b5dd3dbf17b0d671a4cb6fa0f
Steve Mactaggart <steve.mactaggart@open.edu.au>
2014-06-28 16:51:57 +1000

    Reverted my client error testing change.

--- pyrelic/client.py
+++ pyrelic/client.py
@@ -62,7 +62,7 @@ client = pyrelic.Client(account_id='12345', api_key='1234567890abcdef123456789')
         for the user to handle as they see fit.
         """
         status_code = error.response.status_code
-        message = error.message        content = error.response.content
+        message = error.message
 
         if 403 == status_code:
             raise NewRelicInvalidApiKeyException(message)
